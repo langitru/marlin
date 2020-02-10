28b0b98dc0b5d2a8649a2f7cc8e2563929f8b7cc
Andrey Cherepanov <cherepanov2112@gmail.com>
2020-02-10 13:40:36 +0700

    First initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>phpshka.loc - home page</title>
+</head>
+<body>
+	<h1>Hello World!</h1>
+
+
+<?
+
+$arr1 = [];
+$arr2 = ["color" => "red", "year" => 2020];
+
+
+echo 'TEST: ' . $arr2['year'];
+
+?>
+</body>
+</html>
